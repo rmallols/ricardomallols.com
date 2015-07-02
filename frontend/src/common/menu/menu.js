05b5f9cf3244bbcb5c25aa8aac6ea5755f0781e6
rmallols <ricardo.mallols@gmail.com>
2015-07-03 00:27:21 +0100

    Improving color schema across diferent views. Improved the 'where' page.

--- frontend/src/common/menu/menu.js
+++ frontend/src/common/menu/menu.js
@@ -1,14 +1,18 @@
-console.log('MEEE v2!!')
 app.directive('menu', [function () {
     return {
         templateUrl: 'menu.html',
         replace: true,
         link: function (scope) {
+
             scope.links = [
                 {id: 'who', text: 'Who I am'},
                 {id: 'what', text: 'What I do'},
                 {id: 'where', text: 'Where I am'}
             ];
+
+            scope.$on('$stateChangeSuccess', function (event, currentState) {
+               scope.currentState = currentState.name;
+            });
         }
     }
 }]);
--- frontend/src/common/templates.js
+++ frontend/src/common/templates.js
@@ -5,9 +5,12 @@ angular.module("menu.html", []).run(["$templateCache", function($templateCache) 
     "<div class=\"menu\">\n" +
     "    <div class=\"header\"></div>\n" +
     "    <ul class=\"navigation\">\n" +
-    "        <li ng-repeat=\"link in links\" class=\"{{link.id}}-link\">\n" +
-    "            <icon class=\"{{link.id}}-link-icon\"></icon>\n" +
-    "            <a ui-sref=\"{{link.id}}\">{{link.text}}</a>\n" +
+    "        <li ng-repeat=\"link in links\" class=\"link link-{{link.id}}\"\n" +
+    "            ng-class=\"{'link-current': link.id === currentState}\">\n" +
+    "            <icon class=\"link-icon link-icon-{{link.id}}\"></icon>\n" +
+    "            <a ui-sref=\"{{link.id}}\" class=\"link-text link-text-{{link.id}}\">\n" +
+    "                {{link.text}}\n" +
+    "            </a>\n" +
     "        </li>\n" +
     "    </ul>\n" +
     "</div>");
@@ -32,26 +35,34 @@ angular.module("what.html", []).run(["$templateCache", function($templateCache) 
 
 angular.module("where.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("where.html",
-    "<p>\n" +
-    "    <div class=\"col-3 contact-method\">\n" +
+    "<div>\n" +
+    "    <div class=\"col-2 contact-method\">\n" +
     "        <a href=\"mailto:hi@ricardomallols.com\">\n" +
     "            <img src=\"img/mail.svg\"/>\n" +
     "            hi@ricardomallols.com\n" +
     "        </a>\n" +
     "    </div>\n" +
-    "    <div class=\"col-3 contact-method\">\n" +
+    "    <div class=\"col-2 contact-method\">\n" +
     "        <a href=\"http://www.linkedin.com/pub/ricardo-mallols-olmos/20/6a/708\">\n" +
     "            <img src=\"img/linkedin.svg\"/>\n" +
     "            Ricardo Mallols\n" +
     "        </a>\n" +
     "    </div>\n" +
-    "    <div class=\"col-3 contact-method\">\n" +
+    "</div>\n" +
+    "<div>\n" +
+    "    <div class=\"col-2 contact-method\">\n" +
     "        <a href=\"https://twitter.com/#!/RicardoMallols\">\n" +
     "            <img src=\"img/twitter.svg\"/>\n" +
     "            @RicardoMallols\n" +
     "        </a>\n" +
     "    </div>\n" +
-    "</p>");
+    "    <div class=\"col-2 contact-method\">\n" +
+    "        <a href=\"https://github.com/rmallols\">\n" +
+    "            <img src=\"img/github.svg\"/>\n" +
+    "            Rmallols\n" +
+    "        </a>\n" +
+    "    </div>\n" +
+    "</div>");
 }]);
 
 angular.module("who.html", []).run(["$templateCache", function($templateCache) {
@@ -65,14 +76,14 @@ angular.module("who.html", []).run(["$templateCache", function($templateCache) {
     "            <img src=\"img/whoCoding.svg\" class=\"who-coding-img\"/>\n" +
     "        </p>\n" +
     "        <p>\n" +
-    "            <h2 class=\"who-title\">Coding</h2>\n" +
+    "            <h2 class=\"title title-section\">Coding</h2>\n" +
     "            I'm a technology enthusiast who enjoys running any kind of experimental\n" +
     "            stuff on the browser, specially with Javascript, AngularJs, HTML5 and CSS3.\n" +
     "        <p>\n" +
     "    </div>\n" +
     "    <div class=\"col-2\">\n" +
     "        <img src=\"img/whoSports.svg\" class=\"who-sports-img\"/>\n" +
-    "        <h2 class=\"who-title\">(Motor)Sports</h2>\n" +
+    "        <h2 class=\"title title-section\">(Motor)Sports</h2>\n" +
     "        I practice beach volleyball and tennis. I like motor racing,\n" +
     "        particularly F1, DTM, MotoGP and digital Slot. Anki Drive is the future!\n" +
     "    </div>\n" +
